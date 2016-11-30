f1e0611d4a8fdf78ce625ffffee86ad178858595
ugochukwu <ugokingsley5@gmail.com>
2016-11-30 09:45:57 +0000

    forum 3 30/11/16

--- templates/topic.php
+++ templates/topic.php
@@ -1,3 +1,59 @@
 <?php include('includes/header.php');?>
-
+<ul id="topics">
+							<li id="main-topic" class="topic topic">
+								<div class="row">
+									<div class="col-md-2">
+										<div class="user-info">
+											<img class="avatar pull-left" src="img/rename.png"/>
+											<ul>
+												<li><b>Ugoking</b></li>
+												<li>34 posts</li>
+												<li href="profile.php">Profile</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-md-10">
+										<div class="topic-content pull-right">
+											<p>
+												he that dwells in the secret place of the most high shall abide
+												under the shadow of the almighty. I will say of the lord
+												he is my reefuge and my fortress
+											</p>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li id="main-topic" class="topic topic">
+								<div class="row">
+									<div class="col-md-2">
+										<div class="user-info">
+											<img class="avatar pull-left" src="img/rename.png"/>
+											<ul>
+												<li><b>Ugoking</b></li>
+												<li>34 posts</li>
+												<li href="profile.php">Profile</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-md-10">
+										<div class="topic-content pull-right">
+											<p>
+												he that dwells in the secret place of the most high shall abide
+												under the shadow of the almighty. I will say of the lord
+												he is my reefuge and my fortress
+											</p>
+										</div>
+									</div>
+								</div>
+							</li>
+							
+						</ul>
+						<h3>Reply to Topic</h3>
+						<form>
+							<div class="form-group">
+								<label>Topic Body</label>
+								<textarea class="form-control" type="text" rows="10" cols="80" id="reply" name="body"></textarea>
+								<script>CKEDITOR.replace('reply')</script>
+							</div>
+						</form>
 <?php include('includes/footer.php');?>
--- templates/topics.php
+++ templates/topics.php
@@ -1,3 +1,36 @@
 <?php include('includes/header.php');?>
-
+						<ul id="topics">
+							<li class="topics">
+								<div class="row">
+									<div class="col-md-2">
+										<img class="avatar pull-left" src="img/rename.png"/>
+									</div>
+									<div class="col-md-10">
+										<div class="topic-content pull-right">
+											<h3><a href="topic.html">How did you know about Christ?</a></h3>
+											<div class="topic-info">
+												<a href="category.html">Development</a> >> <a href="profile.html">Ugochukwu</a>
+												<span class="badge pull-right">3</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="topics">
+								<div class="row">
+									<div class="col-md-2">
+										<img class="avatar pull-left" src="img/rename.png"/>
+									</div>
+									<div class="col-md-10">
+										<div class="topic-content pull-right">
+											<h3><a href="topic.html">How did you know about Heaven?</a></h3>
+											<div class="topic-info">
+												<a href="category.html">Development</a> >> <a href="profile.html">Ugochukwu</a>
+												<span class="badge pull-right">5</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
 <?php include('includes/footer.php');?>
